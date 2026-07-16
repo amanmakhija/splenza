0409faaee045674f08d5e38d79d48e60eed2eb09
amanmakhija <amanmakhija2205@gmail.com>
2026-07-16 19:00:13 +0530

    updated firebase config to fetch secret correctly

--- backend/src/main/java/com/splitwise/app/config/FirebaseConfig.java
+++ backend/src/main/java/com/splitwise/app/config/FirebaseConfig.java
@@ -1,42 +1,50 @@
 package com.splitwise.app.config;
 
+import java.io.FileInputStream;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.annotation.Configuration;
 
 import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.enabled:false}")
+    private boolean enabled;
+
+    @Value("${firebase.credentials:}")
+    private String credentialsPath;
+
     @PostConstruct
     public void init() throws Exception {
+        if (!enabled) {
+            return;
+        }
 
         if (!FirebaseApp.getApps().isEmpty()) {
             return;
         }
 
-        InputStream stream = getClass()
-                .getClassLoader()
-                .getResourceAsStream("firebase/firebase-admin.json");
-
-        if (stream == null) {
-            throw new IllegalStateException(
-                    "firebase-admin.json not found"
-            );
+        try (InputStream stream
+                = new FileInputStream(credentialsPath)) {
+            FirebaseOptions options
+                    = FirebaseOptions.builder()
+                            .setCredentials(
+                                    GoogleCredentials.fromStream(stream)
+                            )
+                            .build();
+            FirebaseApp.initializeApp(options);
         }
-
-        FirebaseOptions options
-                = FirebaseOptions.builder()
-                        .setCredentials(
-                                GoogleCredentials.fromStream(stream)
-                        )
-                        .build();
-
-        FirebaseApp.initializeApp(options);
-
     }
 
 }
